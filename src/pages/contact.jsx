4648c4fa435b3a1b1e335b33c31fec90e33d1eea
AquarellFantasia <em224342@gmail.com>
2023-05-28 19:26:02 +0200

    Updated contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -28,25 +28,24 @@ const Contact = () => {
 
 					<div className="contact-container">
 						<div className="title contact-title">
-							Let's Get in Touch: Ways to Connect with Me
+							How to get in touch with me?
 						</div>
 
 						<div className="subtitle contact-subtitle">
-							Thank you for your interest in getting in touch with
-							me. I welcome your feedback, questions, and
-							suggestions. If you have a specific question or
-							comment, please feel free to email me directly at
+							Thank you for expressing your interest
+							in reaching out to me. I greatly appreciate
+							your feedback, inquiries, and suggestions.
+							If you have any specific questions or comments,
+							please don't hesitate to email me directly at
 							&nbsp;{" "}
 							<a href={`mailto:${INFO.main.email}`}>
 								{INFO.main.email}
 							</a>
-							. I make an effort to respond to all messages within
-							24 hours, although it may take me longer during busy
-							periods. Alternatively, you can use the contact form
-							on my website to get in touch. Simply fill out the
-							required fields and I'll get back to you as soon as
-							possible. Finally, if you prefer to connect on
-							social media, you can find me on{" "}
+							. I try my best to respond to all messages
+							within a day, but it might take a bit longer
+							during busy times. If you prefer connecting on
+							social media, you can find me on
+							&nbsp;{" "}
 							<a
 								href={INFO.socials.instagram}
 								target="_blank"
@@ -54,10 +53,7 @@ const Contact = () => {
 							>
 								{INFO.socials.instagram}
 							</a>
-							. I post regular updates and engage with my
-							followers there, so don't hesitate to reach out.
-							Thanks again for your interest, and I look forward
-							to hearing from you!
+							. Looking forward to hearing from you!
 						</div>
 					</div>
 
